72d9dcb04af4aa2e53af74b86308d65ce5faf2ff
Rushabh <bhosalerushabh0@gmail.com>
2025-06-28 00:09:21 +0530

    vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SwrProvider } from "@/utils/swr-config";
-import BottomNav from "@/components/bottomNav";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +32,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SwrProvider>{children}</SwrProvider>
+        <SwrProvider>
+          {children}
+          <Analytics />
+        </SwrProvider>
       </body>
     </html>
   );
